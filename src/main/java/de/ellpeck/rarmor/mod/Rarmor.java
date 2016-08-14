9f129d1cfc5bc7b440e0d3fa22b86d3e61ccde36
Ellpeck <megamaximal@gmail.com>
2016-08-14 20:17:25 +0200

    I knew I'd forget this too

--- src/main/java/de/ellpeck/rarmor/mod/Rarmor.java
+++ src/main/java/de/ellpeck/rarmor/mod/Rarmor.java
@@ -30,7 +30,7 @@ import org.apache.logging.log4j.Logger;
 public final class Rarmor{
 
     public static final String MOD_NAME = "Rarmor";
-    public static final String VERSION = "1.10.2-r1";
+    public static final String VERSION = "@VERSION@";
 
     public static final Logger LOGGER = LogManager.getLogger(MOD_NAME);
 
